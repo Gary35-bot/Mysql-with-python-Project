14c5c6d99c8beede67e8b9cd0c27b731c9445404
Lifechoices <gafrica851@gmail.com>
2021-07-07 17:47:46 +0200

    update

--- main.py
+++ main.py
@@ -26,17 +26,32 @@ class MainScreen:
         self.username_entry.place(x=250, y=130)
         self.password_label = Label(mastery, text="Password:", bg="#2e8bc0")
         self.password_label.place(x=250, y=160)
-        self.password_label = Entry(mastery,)
-        self.password_label.place(x=250, y=190)
+        self.password_label1 = Entry(mastery,)
+        self.password_label1.place(x=250, y=190)
 
-        self.btn = Button(mastery, text="Sign in", border=1)
+        self.btn = Button(mastery, text="Sign in", border=1, command=self.sign_in)
         self.btn.place(x=250, y=230)
-        self.label_btn1 = Label(mastery, text="If You Do Not Have An \nAccount Click here.", bg="#3ACF3A" )
+        self.label_btn1 = Label(mastery, text="If You Do Not Have An \nAccount Click here.", bg="#3ACF3A",)
         self.label_btn1.place(x=20, y=150)
-        self.btn1 = Button(mastery, text="Register?", foreground="#2e8bc0", font="bold")
+        self.btn1 = Button(mastery, text="Register?", foreground="#2e8bc0", font="bold",  command=self.listing)
         self.btn1.place(x=50, y=230)
 
+    def sign_in(self):
+        mydb = mysql.connector.connect(user='lifechoices', password='@Lifechoices1234', host='127.0.0.1', database='Lifechoices_Online', auth_plugin='mysql_native_password')
+        mycursor = mydb.cursor()
 
+        xy = mycursor.execute("Select * from CLientele")
+        for i in mycursor:
+            print(i)
+            if self.username_entry.get() == i[0] and self.password_label1.get() == i[2]:
+                messagebox.showinfo("Successful! Enjoy the day")
+
+            elif self.username_entry.get() == i[0] or self.password_label1.get() == i[2]:
+                messagebox.showerror("message: Login unsuccessful")
+
+    def listing(self):
+        root.destroy()
+        import register
 
 
 x = MainScreen(root)
--- register.py
+++ register.py
@@ -1,7 +1,7 @@
-import mysql.connector
 from tkinter import *
 from tkinter import messagebox
 import tkinter. font as font
+import mysql.connector
 
 root = Tk()
 root.title("Register")
@@ -45,6 +45,29 @@ class Register:
         self.mobile_label.place(x=300, y=220)
         self.mobile_entry = Entry(mastery)
         self.mobile_entry.place(x=300, y=260)
+
+        self.btn3 = Button(mastery, text="Register")
+        self.btn3.place(x=350, y=320)
+        self.btn4 = Button(mastery, text="Return to main screen", command=self.back_main)
+        self.btn4.place(x=20, y=320)
+
+    def register(self):
+        mydb = mysql.connector.connect(user='lifechoices', password='@Lifechoices1234', host='127.0.0.1', database='Lifechoices_Online', auth_plugin='mysql_native_password')
+        mycursor = mydb.cursor()
+
+        xy = mycursor.execute("Select * from CLientele")
+        for i in mycursor:
+            print(i)
+            if self.username_entry.get() == i[0] and self.password_label1.get() == i[2]:
+                messagebox.showinfo("Successful! Enjoy the day")
+
+            elif self.username_entry.get() == i[0] or self.password_label1.get() == i[2]:
+                messagebox.showerror("message: Login unsuccessful")
+
+    def back_main(self):
+        root.destroy()
+        import main
+
 
 
 x = Register(root)
